73d1941e0d144fe7893278ed35cee7bafb42cb31
Hao Lam <tuonghao2001@gmail.com>
2024-02-16 14:18:34 -0800

    add users table

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 import os
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, request, redirect
 from flask_cors import CORS
 from flask_migrate import Migrate
-from flask_wtf.csrf import CSRFProtect, generate_csrf
+from flask_wtf.csrf import generate_csrf
 from flask_login import LoginManager
 from .models import db, User
 from .api.user_routes import user_routes
@@ -24,7 +24,6 @@ def load_user(id):
 
 # Tell flask about our seed commands
 app.cli.add_command(seed_commands)
-
 app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,9 @@
-from .db import db, environment, SCHEMA, add_prefix_for_prod
-from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+from sqlalchemy.orm import validates
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from .db import db, environment, SCHEMA
+# from .Message import Message
 
 
 class User(db.Model, UserMixin):
@@ -10,24 +13,85 @@ class User(db.Model, UserMixin):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
-    email = db.Column(db.String(255), nullable=False, unique=True)
-    hashed_password = db.Column(db.String(255), nullable=False)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    hashed_password = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
+    profile_image_url = db.Column(db.String)
+    is_deleted = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.Date, default=datetime.now)
+    updated_at = db.Column(db.Date, default=datetime.now, onupdate=datetime.now)
+
+
+    # """ one-to-many """
+    # user_workspaces = db.relationship("Workspace", back_populates="owner", cascade="all, delete-orphan")
+    # channels = db.relationship("Channel", back_populates="owner")
+    # messages = db.relationship("Message", back_populates="owner", foreign_keys=[Message.sender_id])
+    # reactions = db.relationship("Reaction", back_populates="user")
+
+    # """ many-to-many """
+    # workspaces = db.relationship('Workspace', secondary="memberships", back_populates="users")
+
+
+    @validates('first_name')
+    def validate_first_name(self, _, val):
+        if not len(val):
+            raise ValueError({ "first_name": "First name is required" })
+        return val
+
+
+    @validates('last_name')
+    def validate_last_name(self, _, val):
+        if not len(val):
+            raise ValueError({"last_name": "Last name is required"})
+        return val
+
+
+    @validates('username')
+    def validate_username(self, _, val):
+        if len(val) < 4:
+            raise ValueError({"username": "Username must be at least 4 characters long"})
+        if len([user for user in User.query.all() if user.username == val]):
+            raise ValueError({ "username": "User with that username already exists" })
+        return val
+
+
+    @validates("email")
+    def validate_email(self, _, val):
+        if "@" not in val:
+            raise ValueError({"message": "Invalid email"})
+        if len([user for user in User.query.all() if user.email == val]):
+            raise ValueError({ "email": "User with that email already exists" })
+        return val
+
+
+    @classmethod
+    def username_to_ids(cls):
+        return { user.username: user.id for user in cls.query.all() }
+
 
     @property
     def password(self):
         return self.hashed_password
 
+
     @password.setter
     def password(self, password):
         self.hashed_password = generate_password_hash(password)
 
+
     def check_password(self, password):
         return check_password_hash(self.password, password)
 
+
     def to_dict(self):
         return {
-            'id': self.id,
-            'username': self.username,
-            'email': self.email
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "username": self.username,
+            "email": self.email,
+            "profile_image_url": self.profile_image_url,
+            "is_deleted": self.is_deleted
         }
