1ee9ab073c9a931bfd2b2404a724c2333776d51c
Hao Lam <tuonghao2001@gmail.com>
2024-02-16 15:13:47 -0800

    seed memberhips

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -2,6 +2,7 @@ from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .workspaces import seed_workspaces, undo_workspaces
 from .channels import seed_channels, undo_channels
+from .memberships import seed_memberships, undo_memberships
 from app.models.db import db, environment, SCHEMA
 
 seed_commands = AppGroup('seed')
@@ -29,7 +30,7 @@ def seed_all_tables():
   seed_users()
   seed_workspaces()
   seed_channels()
-#   seed_memberships()
+  seed_memberships()
 #   seed_messages()
 #   seed_reactions()
 
@@ -37,7 +38,7 @@ def seed_all_tables():
 def unseed_all_tables():
 #   undo_reactions()
 #   undo_messages()
-#   undo_memberships()
+  undo_memberships()
   undo_channels()
   undo_workspaces()
   undo_users()
---
+++ app/seeds/memberships.py
@@ -0,0 +1,67 @@
+from sqlalchemy.sql import text
+from ..models import db, User, Workspace
+from ..models.db import environment, SCHEMA
+
+
+def seed_memberships():
+    memberships = [
+      {
+          "username": "haolam",
+          "workspace_name": "aA Union"
+      },
+      {
+          "username": "robin",
+          "workspace_name": "aA Union"
+      },
+      {
+          "username": "zoro",
+          "workspace_name": "aA Union"
+      },
+      {
+          "username": "sanji",
+          "workspace_name": "aA Union"
+      },
+      {
+          "username": "haolam",
+          "workspace_name": "hao-nick-nicky"
+      },
+      {
+          "username": "nickleger",
+          "workspace_name": "hao-nick-nicky"
+      },
+      {
+          "username": "nickylei",
+          "workspace_name": "hao-nick-nicky"
+      },
+      {
+          "username": "haolam",
+          "workspace_name": "hao-nick"
+      },
+      {
+          "username": "mihawk",
+          "workspace_name": "hao-nicky"
+      },
+      {
+          "username": "haolam",
+          "workspace_name": "hao-nicky"
+      },
+      {
+          "username": "haolam",
+          "workspace_name": "nick-nicky"
+      }
+    ]
+
+    for m in memberships:
+        user = User.query.filter(User.username == m['username']).one()
+        workspace = Workspace.query.filter(Workspace.name == m['workspace_name']).one()
+        user.workspaces.append(workspace)
+    db.session.commit()
+
+
+def undo_memberships():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.memberships RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM memberships"))
+
+    db.session.commit()
