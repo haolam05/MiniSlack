9eab47208f5679606199861da2f1afe89e002df8
NickyViNi <happylemonlcm@gmail.com>
2024-02-16 16:52:20 -0800

    sign up route

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -20,8 +20,23 @@ def username_exists(form, field):
         raise ValidationError('Username is already in use.')
 
 
+def email_check(form, field):
+    if "@" not in field.data or "." not in field.data:
+        raise ValidationError('Email is invalid.')
+
+
+def username_check_len(form, field):
+    if len(field.data) < 4:
+         raise ValidationError('Username must be at least 4 characters.')
+
+
+def password_check_len(form, field):
+    if len(field.data) < 6:
+         raise ValidationError('Password must be at least 6 characters.')
+
+
 class SignUpForm(FlaskForm):
     username = StringField(
-        'username', validators=[DataRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), user_exists])
-    password = StringField('password', validators=[DataRequired()])
+        'username', validators=[DataRequired(), username_check_len, username_exists])
+    email = StringField('email', validators=[DataRequired(), email_check, user_exists ])
+    password = StringField('password', validators=[DataRequired(), password_check_len])
