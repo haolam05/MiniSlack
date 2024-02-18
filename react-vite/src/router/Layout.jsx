f34b1f67208756261ea1fa5c77ab7e195e37e5c5
Hao Lam <tuonghao2001@gmail.com>
2024-02-18 12:39:39 -0800

    cleanup modals & delete unecessary pages

--- react-vite/src/router/Layout.jsx
+++ react-vite/src/router/Layout.jsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { ModalProvider, Modal } from "../context/Modal";
+import Navigation from "../components/Navigation";
 import * as sessionActions from "../redux/session";
-import Navigation from "../components/Navigation/Navigation";
 
 export default function Layout() {
   const dispatch = useDispatch();
