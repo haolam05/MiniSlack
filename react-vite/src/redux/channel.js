cf41b07c7d7a71faedc3cf6515541e55b1df379d
NickyViNi <happylemonlcm@gmail.com>
2024-02-19 14:16:54 -0800

    get channels thunk

--- react-vite/src/redux/channel.js
+++ react-vite/src/redux/channel.js
@@ -1,34 +1,61 @@
 import { csrfFetch } from "./csrf";
 
 //Action
-const ADD_CHANNAELS = "channels/addChannels";
+const GET_CHANNELS = "channels/getChannels";
+const ADD_CHANNELS = "channels/addChannels";
 
 
 // POJO action creators
+const getChannelsAction = (channels) => {
+    return {
+        type: GET_CHANNELS,
+        channels
+    }
+}
+
 const addChannelsAction = (channel) => {
     return {
-        type: ADD_CHANNAELS,
+        type: ADD_CHANNELS,
         channel
     }
 }
 
 
 // Thunk action creators
-export const addChannelsThunk = (workspaceId, channel) => async (dispatch) => {
+export const getChannelsThunk = (workspaceId, channel) => async (dispatch) => {
     const res = await csrfFetch(`/api/workspaces/${workspaceId}/channels/`);
     const data = await res.json();
+    if (!res.ok) return { errors: data }
+    dispatch(getChannelsAction(data.Channels))
+}
+
+export const addChannelsThunk = (workspaceId, channel) => async (dispatch) => {
+    const res = await csrfFetch(`/api/workspaces/${workspaceId}/channels/`, {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(channel)
+    });
+    const data = await res.json();
     if (!res.ok) return { errors: data };
-    dispatch(addChannelsAction(channel))
+    dispatch(addChannelsAction(data))
 }
 
 
 // Reducer
-const initialState = {};
+const initialState = { channels: {} };
 
 export default function channelReducer(state = initialState, action) {
     switch (action.type) {
-        case ADD_CHANNAELS:
-            return
+        case GET_CHANNELS: {
+            const allChannels = {};
+            action.channels.forEach(channel => allChannels[channel.id] = channel);
+            return {...state, channels: allChannels}
+        }
+        case ADD_CHANNELS: {
+            const newAllChannels = {...state.channels};
+            newAllChannels[action.id] = action.channel;
+            return {...state, channels: newAllChannels}
+        }
         default:
             return state;
     }
--- react-vite/src/router/index.jsx
+++ react-vite/src/router/index.jsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Layout from './Layout';
 import HomePage from '../components/HomePage';
-import WorkspaceForm from '../components/WorkspaceForm';
 import SomethingWentWrong from '../components/SomethingWentWrong';
 
 export const router = createBrowserRouter([
@@ -12,10 +11,6 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <HomePage />,
-      },
-      {
-        path: "/workspace",
-        element: <WorkspaceForm />
       },
       {
         path: "*",
