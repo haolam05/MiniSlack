2aa6f0319c2cc1f0bb10ee94bfd99da9718b1794
NickyViNi <happylemonlcm@gmail.com>
2024-02-19 14:41:51 -0800

    channel form

---
+++ react-vite/src/components/CreateChannelFromModal/CreateChannelFormModal.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react"
+import ".CreateChannelFormModal.css"
+import { disabledSubmitButton } from "../../utils/dom"
+
+export const CreateChannelFormModal = () => {
+
+    const [name, setName] = useState('');
+    const [topic, setTopic] = useState('');
+    const [description, setDescription] = useState('');
+    const [errors, setErrors] = useState({});
+
+    const submitForm = async (e) => {
+        e.preventDefault()
+        disabledSubmitButton();
+
+
+
+        const payload = {
+            name,
+            topic,
+            description
+        }
+    }
+
+
+
+    return (
+        <div id="create-channel-form-container">
+            <form
+                onSubmit={e => submitForm(e)}
+            >
+                <label>
+                    name
+                    <input
+                        type="text"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                    >
+                    </input>
+                </label>
+                <label>
+                    topic
+                    <input
+                        type="text"
+                        value={topic}
+                        onChange={e => setTopic(e.target.value)}
+                    >
+                    </input>
+                </label>
+                <label>
+                    description
+                    <input
+                        type="text"
+                        value={description}
+                        onChange={e => setDescription(e.target.value)}
+                    >
+                    </input>
+                </label>
+                <button
+                    type="submit"
+                >
+                    Create Channel
+                </button>
+            </form>
+
+        </div>
+    )
+}
