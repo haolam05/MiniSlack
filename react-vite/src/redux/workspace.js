c03dd6e5e964b46b4dfbea9654e8802242e8f76f
Hao Lam <tuonghao2001@gmail.com>
2024-02-19 13:18:13 -0800

    empty redux store when user is deleted/logout/change password

--- react-vite/src/redux/workspace.js
+++ react-vite/src/redux/workspace.js
@@ -3,12 +3,17 @@ import { csrfFetch } from "./csrf";
 
 // Actions
 const ADD_WORKSPACES = 'worksapces/ADD_WORKSPACES'
+const RESET = 'workspaces/RESET';
 
 
 // POJO action creators
 const addWorkspaces = workspaces => ({
   type: ADD_WORKSPACES,
   workspaces
+});
+
+export const reset = () => ({
+  type: RESET
 });
 
 
@@ -42,6 +47,8 @@ function workspaceReducer(state = initialState, action) {
           ...action.workspaces
         }
       }
+    case RESET:
+      return initialState;
     default:
       return state;
   }
