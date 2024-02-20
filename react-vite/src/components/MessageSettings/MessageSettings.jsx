e53f71d60494d53078f372c4f67295414a5e165a
Hao Lam <tuonghao2001@gmail.com>
2024-02-20 10:52:44 -0800

    bugfix delete message ---> temporary hide message on frontend and wait for refresh

--- react-vite/src/components/MessageSettings/MessageSettings.jsx
+++ react-vite/src/components/MessageSettings/MessageSettings.jsx
@@ -11,14 +11,31 @@ function MessageSettings() {
 
   }
 
+  const openUpdateMessageForm = e => {
+    const message = e.target.closest(".message");
+    if (!message) return;
+    const messageId = +message.id;
+
+    console.log(message)
+
+    // setModalContent(
+    //   <ConfirmDeleteFormModal
+    //     text="Are you sure you want to delete this message?"
+    //     deleteCb={() => updateMessage(e, messageId)}
+    //     cancelDeleteCb={closeModal}
+    //   />
+    // );
+  }
+
   const deleteMessage = async (e, messageId) => {
     const data = await dispatch(messageActions.deleteMessageThunk(messageId));
     if (data?.errors) return;
-    e.target.closest(".message.me")?.remove();
+    const message = e.target.closest(".message.me");
+    if (message) message.classList.add("hidden");
     closeModal();
   }
 
-  const openDeleteMessageform = e => {
+  const openDeleteMessageForm = e => {
     const message = e.target.closest(".message");
     if (!message) return;
     const messageId = +message.id;
@@ -34,8 +51,8 @@ function MessageSettings() {
 
   return (
     <div className="message-settings">
-      <span onClick={updateMessage}><i className="fa-solid fa-gear"></i></span>
-      <span onClick={openDeleteMessageform}><i className="fa-solid fa-trash-can"></i></span>
+      <span onClick={openUpdateMessageForm}><i className="fa-solid fa-gear"></i></span>
+      <span onClick={openDeleteMessageForm}><i className="fa-solid fa-trash-can"></i></span>
     </div>
   );
 }
