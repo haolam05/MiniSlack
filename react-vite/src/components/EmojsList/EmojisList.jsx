7f43063da00dfc1b481a1356d4a7ea97251560ca
Hao Lam <tuonghao2001@gmail.com>
2024-02-23 08:49:40 -0800

    searchbox icon-emojis

--- react-vite/src/components/EmojsList/EmojisList.jsx
+++ react-vite/src/components/EmojsList/EmojisList.jsx
@@ -1,14 +1,37 @@
+import { useEffect, useState } from "react";
+
 function EmojisList({ emojis, setMessageInput }) {
+  const [searchEmoji, setSearchEmoji] = useState("");
+  const [currentEmojis, setCurrentEmojis] = useState([...emojis]);
+
+  useEffect(() => {
+    setCurrentEmojis([...emojis]);
+  }, [emojis]);
+
   return (
     <div className="emojis-list hidden">
-      {emojis && (
+      {currentEmojis && (
         <div className="icon-emojis">
-          {emojis.map(emoji => {
+          {!emojis.length ? '' : <div className="searchbox-icons">
+            <input
+              type="text"
+              spellCheck={false}
+              placeholder={`Search for emojis`}
+              value={searchEmoji}
+              onChange={e => {
+                setSearchEmoji(e.target.value);
+                if (e.target.value === "") return setCurrentEmojis(emojis);
+                setCurrentEmojis(emojis.filter(emoji => emoji.unicodeName.slice(5).toLowerCase().includes(e.target.value.toLowerCase())));
+              }}
+            />
+          </div>}
+          {currentEmojis.map(emoji => {
             const codePoint = "0x" + emoji.codePoint.split(" ")[0];
             return (
               <div
                 id={codePoint}
                 className="emoji"
+                title={emoji.unicodeName.slice(5)}
                 key={emoji.slug}
                 onClick={e => setMessageInput(prev => prev + String.fromCodePoint(e.target.id))}
               >
