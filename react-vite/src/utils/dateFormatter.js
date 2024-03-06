fd2f855659d4dc0ad366058c1ae9c4489ef0e123
Hao Lam <tuonghao2001@gmail.com>
2024-03-06 15:08:04 -0800

    bugfix datetime

--- react-vite/src/utils/dateFormatter.js
+++ react-vite/src/utils/dateFormatter.js
@@ -1,15 +1,15 @@
 export const formattedDate = inputDate => {
-  const parts = inputDate.split(" ");
-  if (parts.length !== 2) return;
-  const date = parts[0];
-  const [year, month, day] = date.split("-");
-  return `${year}-${`${month}`.padStart(2, '0')}-${`${day}`.padStart(2, '0')}`;
+  const date = new Date(inputDate);
+  const year = date.getUTCFullYear().toString().padStart(4, '0');
+  const month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
+  const day = date.getUTCDate().toString().padStart(2, '0');
+  return `${year}-${month}-${day}`;
 };
 
 export const formattedTime = inputDate => {
-  const parts = inputDate.split(" ");
-  if (parts.length !== 2) return;
-  const time = parts[1].split('.')[0];
-  const [hour, min, sec] = time.split(":");
-  return `${hour.padStart(2, '0')}:${min.padStart(2, '0')}:${sec.padStart(2, '0')}`;
+  const date = new Date(inputDate);
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
+  return `${hours}:${minutes}:${seconds}`;
 };
