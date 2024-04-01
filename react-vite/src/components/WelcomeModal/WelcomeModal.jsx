01a7909abce01d6b3b951ce981306d784971f308
Hao Lam <tuonghao2001@gmail.com>
2024-04-01 08:48:02 -0700

    added team members info & social

--- react-vite/src/components/WelcomeModal/WelcomeModal.jsx
+++ react-vite/src/components/WelcomeModal/WelcomeModal.jsx
@@ -18,6 +18,25 @@ const WelcomeModal = () => {
             <p>Let&apos;s unleash your collaborative spirit! This platform lets you create unlimited workspaces, inviting friends, family, classmates, and colleagues to join for free. Dive into engaging topics within dedicated channels. As the workspace owner, you hold the reins: update or delete the workspace, send invites, manage channels, and even remove members. Worried about typos? No sweat! Easily edit or delete messages. Express yourself with reactions, and discover even more exciting features waiting to be explored. Let&apos;s create something amazing, together!</p>
             <br />
             <p>Ready to unlock deeper connections? Join us today!</p>
+            <br />
+            <br />
+            <div className="team-info">
+                <div>Contact Us 📳</div>
+                <div className="team-members">
+                    <a className="team-member" target="_blank" href="https://www.linkedin.com/in/hao-lam-378065196/">
+                        <span><i className="fa-brands fa-linkedin"></i></span>
+                        <span>Hao Lam</span>
+                    </a>
+                    <a className="team-member" target="_blank" href="https://www.linkedin.com/in/nicky-li/">
+                        <span><i className="fa-brands fa-linkedin"></i></span>
+                        <span>Nicky Li</span>
+                    </a>
+                    <a className="team-member" target="_blank" href="https://www.linkedin.com/in/nick-leger-a3523a109/">
+                        <span><i className="fa-brands fa-linkedin"></i></span>
+                        <span>Nick Leger</span>
+                    </a>
+                </div>
+            </div>
         </div>
     )
 }
